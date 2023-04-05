5725c2b574348ac0a6f56fd1f72ff7adf8b5c1ab
otaku.prof <norman_lee@sutd.edu.sg>
2023-04-05 12:19:21 +0800

    initial commit

---
+++ app/src/main/java/com/example/dummyfinalexampart1starterrepository/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.dummyfinalexampart1starterrepository;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
